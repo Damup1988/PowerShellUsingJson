d3ec005026a3692f0c0c4bf3c13a99e05819303b
Damup1988 <damup1988@gmail.com>
2021-04-27 02:21:17 +0300

    27.04.2021 commit

--- use-json.ps1
+++ use-json.ps1
@@ -1,1 +1,11 @@
 #using json settings in powershell
+
+$data = Import-Csv -Path "D:\_ONEDRIVE\OneDrive\_Coding\_Projects\PowerShell\PowerShellAndJson\data.csv" -Delimiter ';'
+$data | ConvertTo-Json | Add-Content -Path 'out.json'
+
+$json_data = Get-Content -Path 'out.json'
+$data2 = $json_data | ConvertFrom-Json
+
+foreach ($item in $data2) {
+    $item.name 
+}
